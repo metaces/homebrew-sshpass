d25828b9d5bb1e9030f5739a63ac7b49fd26d69c
Cesar Felipe Reis Alberto <metaces@gmail.com>
2020-07-21 13:18:29 -0300

    Update sshpass.rb

--- sshpass.rb
+++ sshpass.rb
@@ -1,8 +1,8 @@
 class Sshpass < Formula
-  homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
+  url "http://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz"
+  homepage "http://sourceforge.net/projects/sshpass"
   sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
-
+  
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
